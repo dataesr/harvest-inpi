7aad23b058f66e9a3fe5b0b3465f50c4512d9cf2
juliaGrandhay <julia.grandhay@gmail.com>
2023-06-05 15:05:38 +0200

    remove 3rd function harvest-inpi in tasks

--- application/server/main/tasks.py
+++ application/server/main/tasks.py
@@ -1,7 +1,7 @@
 import os
 
 from application.server.main.logger import get_logger
-from inpi import p00_ftp_inpi, p01_unzip_inpi, p02_lecture_xml
+from inpi import p00_ftp_inpi, p01_unzip_inpi
 
 logger = get_logger(__name__)
 
@@ -23,7 +23,5 @@ def harvest_inpi():
     p00_ftp_inpi.loading()
     logger.debug("chargement de la dernière version complète de la DB de l'INPI")
     p01_unzip_inpi.unzip()
-    logger.debug("dezippage des fichiers zippés et chargement dans ObjectStorage")
-    p02_lecture_xml.update_db()
-    logger.debug("alimentation de la base mongo inpi")
+    logger.debug("dezippage des fichiers zippés, chargement dans ObjectStorage et base mongo INPI")
 
