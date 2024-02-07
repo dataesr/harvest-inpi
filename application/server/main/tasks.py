c9ec131f6f181df23e35f59cc421522935476802
victor barbier <victor.barbier@recherche.gouv.fr>
2024-02-07 14:35:29 +0100

    feat(tasks): new tasks

--- application/server/main/tasks.py
+++ application/server/main/tasks.py
@@ -2,26 +2,81 @@ import os
 
 from application.server.main.logger import get_logger
 from inpi import p00_ftp_inpi, p01_unzip_inpi
+from inpi.load import mongo_load
 
 logger = get_logger(__name__)
 
-DATA_PATH = os.getenv('MOUNTED_VOLUME_TEST')
+DATA_PATH = os.getenv("MOUNTED_VOLUME_TEST")
 
 
-def create_task_clean(args):
-    os.system(f'rm -rf {DATA_PATH}')
+def task_disk_clean(args):
+    os.system(f"rm -rf {DATA_PATH}")
 
 
-def create_task_all(args):
-    # if args.get('recuperation', True):
-    #     create_recuperation()
-    if args.get('harvest-inpi', True):
-        harvest_inpi()
-
-
-def harvest_inpi():
+def task_download_inpi(args):
+    # Load INPI database from ftp
     p00_ftp_inpi.loading()
-    logger.debug("chargement de la dernière version complète de la DB de l'INPI")
-    p01_unzip_inpi.unzip()
-    logger.debug("dezippage des fichiers zippés, chargement dans ObjectStorage et base mongo INPI")
 
+
+def task_unzip_inpi(args):
+    # Unzip files
+    p01_unzip_inpi.unzip()
+
+
+def task_download_and_unzip_inpi(args):
+    # Load INPI database from ftp
+    p00_ftp_inpi.loading()
+
+    # Unzip files
+    p01_unzip_inpi.unzip()
+
+
+def task_mongo_load(args):
+    # Extract and load mongo
+    mongo_load()
+
+
+def task_mongo_load_with_history(args):
+    # Extract and load mongo
+    mongo_load(with_history=True)
+
+
+def task_mongo_load_force(args):
+    # Extract and load mongo
+    mongo_load(force=True, force_years=args.get("force_years"))
+
+
+def task_mongo_load_force_with_history(args):
+    # Extract and load mongo
+    mongo_load(force=True, force_years=args.get("force_years"))
+
+
+def task_mongo_reload(args):
+    # Extract and load mongo
+    mongo_load(reset_mongo=True)
+
+
+def task_mongo_reload_with_history(args):
+    # Extract and load mongo
+    mongo_load(with_history=True, reset_mongo=True)
+
+
+def task_mongo_reload_force(args):
+    # Reset mongo, extract and load mongo
+    mongo_load(force=True, force_years=args.get("force_years"), reset_mongo=True)
+
+
+def task_mongo_reload_force_with_history(args):
+    # Reset mongo, extract and load mongo
+    mongo_load(with_history=True, force=True, force_years=args.get("force_years"), reset_mongo=True)
+
+
+def task_harvest_inpi(args):
+    # Load INPI database from ftp
+    p00_ftp_inpi.loading()
+
+    # Unzip files
+    p01_unzip_inpi.unzip()
+
+    # Extract and load mongo
+    mongo_load()
--- application/server/main/views.py
+++ application/server/main/views.py
@@ -1,47 +1,114 @@
-import json
-import os
-import pandas
-import requests
 import redis
-
 from flask import Blueprint, current_app, jsonify, render_template, request
 from rq import Connection, Queue
-from application.server.main.tasks import create_task_all, create_task_clean
+
+import application.server.main.tasks as tasks
 
 default_timeout = 43200000
 
-main_blueprint = Blueprint('main', __name__, )
+main_blueprint = Blueprint(
+    "main",
+    __name__,
+)
 
 
-
-@main_blueprint.route('/', methods=['GET'])
+@main_blueprint.route("/", methods=["GET"])
 def home():
-    return render_template('home.html')
+    return render_template("home.html")
 
 
-@main_blueprint.route('/harvest_compute', methods=['POST'])
-def run_task_all():
+@main_blueprint.route("/harvest_compute", methods=["POST"])
+def run_task_harvest_inpi():
     """
-    All processes for patents
-    """
-    args = request.get_json(force=True)
-
-    with Connection(redis.from_url(current_app.config['REDIS_URL'])):
-        q = Queue(name='inpi', default_timeout=default_timeout)
-        task = q.enqueue(create_task_all, args)
-    response_object = {'status': 'success', 'data': {'task_id': task.get_id()}}
-    return jsonify(response_object), 202
-
-@main_blueprint.route('/clean', methods=['POST'])
-def run_task_clean():
-    """
-    All processes for patents
+    Harvest of the INPI database.
     """
     args = request.get_json(force=True)
-    with Connection(redis.from_url(current_app.config['REDIS_URL'])):
-        q = Queue(name='inpi', default_timeout=default_timeout)
-        task = q.enqueue(create_task_clean, args)
-    response_object = {'status': 'success', 'data': {'task_id': task.get_id()}}
+
+    with Connection(redis.from_url(current_app.config["REDIS_URL"])):
+        q = Queue(name="inpi", default_timeout=default_timeout)
+        task = q.enqueue(tasks.task_harvest_inpi, args)
+    response_object = {"status": "success", "data": {"task_id": task.get_id()}}
     return jsonify(response_object), 202
 
 
+@main_blueprint.route("/harvest_compute_split", methods=["POST"])
+def run_task_harvest_inpi_split():
+    """
+    Harvest of the INPI database with split jobs.
+    """
+    args = request.get_json(force=True)
+    download_task = []
+
+    # Download and unzip inpi db
+    with Connection(redis.from_url(current_app.config["REDIS_URL"])):
+        q = Queue(name="inpi", default_timeout=default_timeout)
+        task = q.enqueue(tasks.task_download_and_unzip_inpi, args)
+        download_task.append(task)
+    response_object = {"status": "success", "data": {"task_id": task.get_id()}}
+
+    # Load mongo without history
+    with Connection(redis.from_url(current_app.config["REDIS_URL"])):
+        q = Queue(name="inpi", default_timeout=default_timeout)
+        task = q.enqueue(tasks.task_mongo_load, args, depends_on=download_task)
+        download_task.append(task)
+    response_object = {"status": "success", "data": {"task_id": task.get_id()}}
+
+    # Load mongo with history
+    with Connection(redis.from_url(current_app.config["REDIS_URL"])):
+        q = Queue(name="inpi", default_timeout=default_timeout)
+        task = q.enqueue(tasks.task_mongo_load_with_history, args, depends_on=download_task)
+    response_object = {"status": "success", "data": {"task_id": task.get_id()}}
+
+    return jsonify(response_object), 202
+
+
+@main_blueprint.route("/mongo_reload", methods=["POST"])
+def run_task_mongo_reload():
+    """
+    Reload of the mongo db.
+    """
+    args = request.get_json(force=True)
+
+    with Connection(redis.from_url(current_app.config["REDIS_URL"])):
+        q = Queue(name="inpi", default_timeout=default_timeout)
+        task = q.enqueue(tasks.task_mongo_reload, args)
+    response_object = {"status": "success", "data": {"task_id": task.get_id()}}
+
+    with Connection(redis.from_url(current_app.config["REDIS_URL"])):
+        q = Queue(name="inpi", default_timeout=default_timeout)
+        task = q.enqueue(tasks.task_mongo_reload_with_history, args)
+    response_object = {"status": "success", "data": {"task_id": task.get_id()}}
+
+    return jsonify(response_object), 202
+
+@main_blueprint.route("/mongo_reload_force", methods=["POST"])
+def run_task_mongo_reload_force():
+    """
+    Forced reload of the mongo db.
+    """
+    args = request.get_json(force=True)
+
+    with Connection(redis.from_url(current_app.config["REDIS_URL"])):
+        q = Queue(name="inpi", default_timeout=default_timeout)
+        task = q.enqueue(tasks.task_mongo_reload_force, args)
+    response_object = {"status": "success", "data": {"task_id": task.get_id()}}
+
+    with Connection(redis.from_url(current_app.config["REDIS_URL"])):
+        q = Queue(name="inpi", default_timeout=default_timeout)
+        task = q.enqueue(tasks.task_mongo_reload_force_with_history, args)
+    response_object = {"status": "success", "data": {"task_id": task.get_id()}}
+
+    return jsonify(response_object), 202
+
+
+@main_blueprint.route("/clean", methods=["POST"])
+def run_task_disk_clean():
+    """
+    Clean all data from disk.
+    """
+    args = request.get_json(force=True)
+    with Connection(redis.from_url(current_app.config["REDIS_URL"])):
+        q = Queue(name="inpi", default_timeout=default_timeout)
+        task = q.enqueue(tasks.task_disk_clean, args)
+    response_object = {"status": "success", "data": {"task_id": task.get_id()}}
+    return jsonify(response_object), 202
